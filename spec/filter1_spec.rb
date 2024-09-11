4d53d21447528a0c0d3c135018e76de08e78ce21
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2024-09-12 01:25:13 +0200

    Add example spec files from RSpec tutorial.

---
+++ spec/filter1_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe "An Example Group with positive and negative Examples" do 
+  context 'when testing Ruby\'s build-in math library' do
+     
+     it 'can do normal numeric operations' do 
+        expect(1 + 1).to eq(2) 
+     end 
+     
+     it 'generates an error when expected' do
+        expect{1/0}.to raise_error(ZeroDivisionError) 
+     end
+     
+  end 
+end
