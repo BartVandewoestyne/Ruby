9b3d7ff63170566b1bd9805fd1da07ab21caf580
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2024-09-12 09:29:51 +0200

    Add example that  uses pry.

---
+++ test.rb
@@ -0,0 +1,22 @@
+# Program to illustrate how pry works.
+#
+# References:
+#   https://pry.github.io/
+
+# test.rb
+require 'pry'
+
+class A
+  def hello() puts "hello world!" end
+end
+
+a = A.new
+
+# set x to 10
+x = 10
+
+# start a REPL session
+binding.pry
+
+# program resumes here (after pry session)
+puts "program resumes here. Value of x is: #{x}."
